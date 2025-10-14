9727faaf3a2d8525afb7cdd3d7091d353822bef8
Pooja Prasad <pprasad1@ualberta.ca>
2025-10-13 22:49:43 -0600

    update models.py

--- teamGold/golden/models.py
+++ teamGold/golden/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    pass
+
+class Entry(models.Model):
+    pass
+
+class Comments(models.Model):
+
+    post = models.ForeignKey(Entry, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="comments")
+    reply_to = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
+    comment_string = models.TextField()
+    likes = models.ManyToManyField(Author, blank=True, related_name='liked_comments')
+    posted = models.DateTimeField(auto_now_add=True)
+
+
+class Node(models.Model):
+    pass

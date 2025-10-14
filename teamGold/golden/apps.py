320f705e60f82053987b249ae42d3df858d24839
Pooja Prasad <pprasad1@ualberta.ca>
2025-10-13 22:24:45 -0600

    updated with new app name

--- teamGold/golden/apps.py
+++ teamGold/golden/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GoldfeedConfig(AppConfig):
+class GoldenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GoldFeed'
+    name = 'golden'
--- teamGold/teamGold/asgi.py
+++ teamGold/teamGold/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gold project.
+ASGI config for teamGold project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gold.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamGold.settings')
 
 application = get_asgi_application()

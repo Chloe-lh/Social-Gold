cbe66c390d11accc1c4c8e5c1fbb387b9037db72
zdsouza1979 <zdsouza@ualberta.ca>
2025-10-20 01:10:30 -0600

    working form for name, github and profile photo

--- teamGold/golden/models.py
+++ teamGold/golden/models.py
@@ -50,7 +50,9 @@ class Author(AbstractBaseUser):
     host  = models.URLField(blank=True)
     github = models.URLField(blank=True)
     web = models.URLField(blank=True)
-    profileImage = models.URLField(default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBkA9WO3FnL4fddebhCcTztCr6vr2METdo9w&s")
+    profileImage = models.ImageField(
+        default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBkA9WO3FnL4fddebhCcTztCr6vr2METdo9w&s",
+        upload_to='profile_pics/')
     username = models.CharField(max_length=50, unique=True, default="goldenuser")
     password = models.CharField(max_length=50, default="goldenpassword")
     is_staff = models.BooleanField(default=False)

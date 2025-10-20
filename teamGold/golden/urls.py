c02238705b92bc1f500595cc51e8c1ca7a7da9fd
Pooja Prasad <pprasad1@ualberta.ca>
2025-10-19 20:53:03 -0600

    follower requests done

--- teamGold/golden/urls.py
+++ teamGold/golden/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('search/', views.search_authors, name='search_authors'),
     path('followers/', views.followers, name='followers'),
     path('following/', views.following, name='following'),
-    path("home/", views.home, name="home")
+    path("follow_requests/", views.follow_requests, name="follow_requests"),
+    path("home/", views.home, name="home"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
     
 

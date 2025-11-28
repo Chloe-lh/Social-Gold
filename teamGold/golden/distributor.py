7a579996089cbef9a8ccbf1509b3fde0d89d6d99
Pooja Prasad <pprasad1@ualberta.ca>
2025-11-28 05:39:32 -0700

    fixed recipient spelling

--- teamGold/golden/distributor.py
+++ teamGold/golden/distributor.py
@@ -65,7 +65,7 @@ def safe_parse_datetime(value):
 
 def send_activity_to_inbox(recipient: Author, activity: dict):
     print("RUNNING UPDATED CODE")
-    print(f"[DEBUG send_activity_to_inbox] Called: recipient= recipient={recepient.username} (id={recepient.id} host={recipient.host})")
+    print(f"[DEBUG send_activity_to_inbox] Called: recipient= {recipient.username} (id={recipient.id} host={recipient.host})")
     print(f"[DEBUG send_activity_to_inbox] Activity type: {activity.get('type')}")
     print("[DEBUG] type(recipient.host) =", type(recipient.host))
     print("[DEBUG] recipient.host =", recipient.host)
